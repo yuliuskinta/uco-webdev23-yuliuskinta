3f43808a0280d981b984ae45a3957e42fd8aea24
yuliuskinta <ykanisia@php.ciputra.ac.id>
2024-11-21 14:45:18 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+Route::prefix('products')->controller(ProductController::class)->group(function(){
+    Route::get('/','index');
+    Route::get('/show/{id}', 'show');
+    Route::get('/create',  'create');
+    Route::get('/edit', 'edit');
 });
-//HelloWorld
+
